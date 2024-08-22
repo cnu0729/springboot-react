376b53af0cb517f1ea25acc99c3b6b8049aa6131
cnu0729 <smile-shinwoo@naver.com>
2024-08-22 12:28:15 +0900

    chicken update delete

--- JPA_chicken/src/main/java/com/kh/dto/Chicken.java
+++ JPA_chicken/src/main/java/com/kh/dto/Chicken.java
@@ -9,6 +9,7 @@ import lombok.*;
 @Entity // mysql에 테이블이 존재하지 않으면 테이블 생성
 @Getter // 룸북이나 자카르타가 나오지 않음 -> build.gradle에 spring이나 lombok 설정 있는지 확인
 @Setter
+@ToString
 public class Chicken {
 	
 	@Id // primary key
