f0ce99c830b93d663d23af7990b80b2f7f6ae448
cnu0729 <smile-shinwoo@naver.com>
2024-08-23 11:59:35 +0900

    검색기능

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -1,23 +1,22 @@
-/*
-ChickenList path="/"
-
-ChickenDetail.js path="/chicken-detail"
-*/
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from'react-router-dom';
 import ChickenList from "./component/ChickenList";
 import ChickenDetail from "./component/ChickenDetail";
 import MainRouter from "./MainRouter";
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainRouter />} />
-        {/* Routes 안에는 Route로 설정된 태그만 들어올 수 있음 <MainRouter /> */}
-        <Route path="/chicken-detail/:id" element={<ChickenDetail />} />
-      </Routes>
-    </Router>
-  );
+import SearchResult from "./component/SearchResult";
+import './App.css';
+function App () {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainRouter />} />
+               {/* Routes 안에는 Route로 설정된 태그만 들어올 수 있음    <MainRouter /> */}
+                <Route path="/chicken-detail/:id" element={<ChickenDetail />} />
+                <Route path="/search" element={<SearchResult />} />
+            </Routes>
+
+        </Router>
+    )
 }
 
 export default App;
